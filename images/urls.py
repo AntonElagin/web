5ae56af96859dc6aba96dac3ca37ab04d71e6299
AntonElagin <anton120299@gmail.com>
2019-11-05 22:45:05 +0300

    refactored

--- images/urls.py
+++ images/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.images_post),
     path('search/', views.images),
-    path('<int:pk>/', views.images_detail),
+    path('<int:image_pk>/', views.images_detail),
     path('<int:image_pk>/comments/', views.comments),
     path('<int:image_pk>/comments/<int:comment_pk>/', views.comments_detail),
     path('<int:image_pk>/likes/', views.likes),
--- images/views.py
+++ images/views.py
@@ -7,7 +7,6 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth.models import User
 from .models import Image, Comment, Like
 from users.models import Creator
-from rest_framework_social_oauth2.authentication import SocialAuthentication
 from .serializers import ImageSerializer, CommentSerializer, LikeSerializer
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework_social_oauth2.authentication import SocialAuthentication
@@ -15,69 +14,65 @@ from oauth2_provider.contrib.rest_framework import OAuth2Authentication
 from users.views import CsrfExemptSessionAuthentication
 
 
-
 @api_view(['GET'])
 @authentication_classes([OAuth2Authentication, SocialAuthentication, CsrfExemptSessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
-@csrf_exempt
 def images(request):
 
      if request.method == 'GET':
         user = User.objects.get(username=request.user.get_username())
-
         creator = Creator.objects.get_or_create(user=user, name=str(user.first_name + user.last_name))
-
         images = Image.objects.filter(creator=creator[0])
 
         # Paginator
         paginator = Paginator(images, 20)
-
         if 'page' in request.GET:
-            page_num = request.GET['page']
+            page_number = request.GET['page']
         else:
-            page_num = 1
-        page = paginator.get_page(page_num)
+            page_number = 1
+        page = paginator.get_page(page_number)
         previous_page = page.previous_page_number() if page.has_previous() else 1
         next_page = page.next_page_number() if page.has_next() else 1
 
         serializer = ImageSerializer(page.object_list, many=True)
         return Response({'data': serializer.data,
-                         'page': page_num,
+                         'page': page_number,
                          'previous': previous_page,
                          'next': next_page,
                          'num_pages': paginator.num_pages})
 
-@api_view(['GET','POST'])
+
+@api_view(['GET', 'POST'])
 @authentication_classes([OAuth2Authentication, SocialAuthentication, CsrfExemptSessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
 def images_post(request):
     user = User.objects.get(username=request.user.get_username())
-
     creator = Creator.objects.get_or_create(user=user, name=str(user.first_name + user.last_name))
-    if request.method == 'GET':
-        images = Image.objects.filter(creator=creator[0])
 
+    if request.method == 'GET':
+
+        images = Image.objects.filter(creator=creator[0])
         # Paginator
         paginator = Paginator(images, 20)
 
         if 'page' in request.GET:
-            page_num = request.GET['page']
+            page_number = request.GET['page']
         else:
-            page_num = 1
-        page = paginator.get_page(page_num)
+            page_number = 1
+        page = paginator.get_page(page_number)
         previous_page = page.previous_page_number() if page.has_previous() else 1
         next_page = page.next_page_number() if page.has_next() else 1
 
         serializer = ImageSerializer(page.object_list, many=True)
         return Response({'data': serializer.data,
-                         'page': page_num,
+                         'page': page_number,
                          'previous': previous_page,
                          'next': next_page,
                          'num_pages': paginator.num_pages})
 
     if request.method == 'POST':
-
         serializer = ImageSerializer(data=request.data)
+
         if serializer.is_valid():
             serializer.creator_id = creator[0].id
             serializer.save()
@@ -86,8 +81,10 @@ def images_post(request):
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def images_detail(request, pk):
-    image = Image.objects.get(pk=pk)
+@authentication_classes([OAuth2Authentication, SocialAuthentication, CsrfExemptSessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def images_detail(request, image_pk):
+    image = Image.objects.get(pk=image_pk)
     if request.method == 'GET':
         serializer = ImageSerializer(image)
         return Response(serializer.data)
@@ -110,10 +107,12 @@ def comments(request, image_pk):
     if request.method == 'POST':
         creator = Creator.objects.get(user__username=request.user)
         serializer = CommentSerializer(data=request.data)
+
         if serializer.is_valid():
             serializer.creator_id = creator.id
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -149,6 +148,7 @@ def likes(request, image_pk):
 @permission_classes([AllowAny])
 def likes_detail(request, pk):
     like = Like.objects.get(pk=pk)
+
     if request.method == 'DELETE':
         like.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
