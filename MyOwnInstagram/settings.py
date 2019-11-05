9e9cbfbf75f54224814101182f338bde6916c7ce
AntonElagin <anton120299@gmail.com>
2019-11-05 15:17:53 +0300

    locale

--- MyOwnInstagram/settings.py
+++ MyOwnInstagram/settings.py
@@ -54,7 +54,7 @@ SECRET_KEY = 'et*nis1ahxzs*pmrsm0aq(rj59_^809bd3q8^cmj2055boux2u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'anton1202.pythonanywhere.com']
+# ALLOWED_HOSTS = [u'anton1202.pythonanywhere.com']
 
 
 # Application definition
@@ -166,7 +166,7 @@ STATIC_URL = '/static/'
 # Facebook configuration
 SOCIAL_AUTH_FACEBOOK_KEY = '2333798976907567'
 SOCIAL_AUTH_FACEBOOK_SECRET = '8ab536630107f393f20c381ed0d01877'
-SOCIAL_AUTH_LOGIN_REDIRECT_URL = 'https://anton1202.pythonanywhere.com/users/images'
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/users/images'
 
 # # Define SOCIAL_AUTH_FACEBOOK_SCOPE to get extra permissions from facebook. Email is not sent by default, to get it, you must request the email permission:
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
