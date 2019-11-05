e90abde790670ad59791cde9675dae7e805e510c
AntonElagin <anton120299@gmail.com>
2019-11-05 16:08:55 +0300

    admin register

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Image, Tag, Comment, Like
 # Register your models here.
+
+
+admin.site.register(Image)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(Like)
+
